074d9001aa8ddf472145a03f9c0eac165c443743
Asliddin Bozorov <asliddiniut@gmail.com>
2023-05-06 12:26:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
+const App = () => {
+  
+  return <main>
+    <Navbar />
+    <Hero />
+    <Sidebar />
+    <Submenu />
+  </main>
+};
+export default App;
